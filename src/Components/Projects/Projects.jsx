873ace620417e21f3da5ad2f670e09bf2f895fbd
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-10-11 19:39:17 +0530

    Updated the project page.

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import "./Projects.css";
 // import { FaReact, FaHardHat } from "react-icons/fa";
 import {
-//   SiBlockchaindotcom,
-//   SiHtml5,
+  //   SiBlockchaindotcom,
+  //   SiHtml5,
   SiMaterialui,
-//   SiChai,
-//   SiEthereum,
+  //   SiChai,
+  //   SiEthereum,
   SiExpress,
-//   SiSocketdotio,
+  //   SiSocketdotio,
   SiTailwindcss,
-  SiReactrouter,
-//   SiJavascript,
+  // SiReactrouter,
+  //   SiJavascript,
   SiMongodb,
-  SiNodedotjs
+  SiNodedotjs,
+  SiNetlify,
+  SiReact,
+  SiRedux,
+  SiRazorpay,
+  SiVercel,
+  // SiRender,
 } from "react-icons/si";
 import { SiHtml5 } from "react-icons/si";
 import { DiCss3 } from "react-icons/di";
@@ -30,22 +36,21 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src="pulse-plus.png"
-                  alt="pulse-plus"
-                />
+                <img src="pulse-plus.png" alt="pulse-plus" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h2>Pulse-Plus Clone</h2>
               <p>
-                Clone of the popular Pulse- plus web application to buy medical products. Built on javascript for front end. It's a team project completed executed in 5 days.
+                Clone of the popular Pulse- plus web application to buy medical
+                products. Built on javascript for front end. It's a team project
+                completed executed in 5 days.
               </p>
               <div>
-               
                 <SiHtml5 />
                 <IoLogoJavascript />
                 <DiCss3 />
+                <SiNetlify />
               </div>
               <div>
                 <a
@@ -58,7 +63,7 @@ export const Projects = () => {
                   </span>
                 </a>
                 <a
-                  href="https://github.com/Shreyasgkhakal100/www.pulseplus.in-Clone"
+                  href="https://github.com/khushboo-choudhary/www.pulseplus.in-Clone"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -74,28 +79,31 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src="tatacliq.jpeg"
-                  alt="TataCliq"
-                />
+                <img src="tatacliq.jpeg" alt="TataCliq" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h2>TataCliq Clone</h2>
               <p>
-              A website to purchase fashion-related products. A collaborative project built by a team of 5 executed in 5 days.
+                A website to purchase fashion-related products. A collaborative
+                project built by a team of 5 executed in 5 days.
               </p>
               <div>
                 <IoLogoJavascript />
                 <DiCss3 />
                 <SiHtml5 />
-                <SiExpress/>
-                <SiMongodb/>
-                <SiNodedotjs/>
+                <SiExpress />
+                <SiMongodb />
+                <SiNodedotjs />
+                {/* <SiRender /> */}
               </div>
               <div>
                 <a
+<<<<<<< Updated upstream
                   href="https://optimistic-fermat-8bc5e2.netlify.app/index.html"
+=======
+                  href="https://tatacliq-website.onrender.com/"
+>>>>>>> Stashed changes
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -120,31 +128,28 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src="rockstar.png"
-                  alt="rockstar"
-                />
+                <img src="rockstar.png" alt="rockstar" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h2>Store.rockstargames </h2>
               <p>
-                Built the rockstoregames application using React. The google authentication and payment functionalities are implemented.
+                Built the rockstoregames application using React. The google
+                authentication and payment functionalities are implemented.
               </p>
               <div>
                 <IoLogoJavascript />
-                <SiMaterialui/>
-                <SiExpress/>
-                <DiCss3 />
-                <SiHtml5 />
-                <SiTailwindcss/>
-                <SiReactrouter/>
-                <SiMongodb/>
-                <SiNodedotjs/>
+                <SiMaterialui />
+                <SiExpress />
+                <SiReact />
+                <SiRedux />
+                <SiTailwindcss />
+                <SiMongodb />
+                <SiNodedotjs />
               </div>
               <div>
                 <a
-                  href="https://store-rockstargames.vercel.app/"
+                  href="https://store-rockstartgames-com-frontend-clone.vercel.app/"
                   target="_blank"
                   rel="noreferrer"
                 >
@@ -169,19 +174,63 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src="youTube.png"
-                  alt="youTube"
-                />
+                <img src="aglecha_diamonds.png" alt="rockstar" />
+              </div>
+            </div>
+            <div className="project_information" data-aos="fade-right">
+              <h2>Aglecha Diamonds </h2>
+              <p>
+                Built the Aglecha diamonds application using React. The google
+                authentication and payment functionalities are implemented.
+              </p>
+              <div>
+                <SiMaterialui />
+                <SiExpress />
+                <SiReact />
+                <SiRedux />
+                <SiVercel />
+                <SiMongodb />
+                <SiNodedotjs />
+                <SiRazorpay />
+              </div>
+              <div>
+                <a
+                  href="https://aglecha-diamond-frontend.vercel.app/"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span type="button" className="btn btn--outline onbt">
+                    URL for the API
+                  </span>
+                </a>
+                <a
+                  href="https://github.com/khushboo-choudhary/aglecha_diamond_frontend"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span type="button" className="btn btn--outline">
+                    View Code
+                  </span>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="projects_container">
+          <div className="project">
+            <div className="project_videocontainer">
+              <div data-aos="fade-right">
+                <img src="youTube.png" alt="youTube" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h2>YouTube Clone</h2>
               <p>
-                Mini project of Youtube clone built using ES6. Implemented import and export and debouncing.
+                Mini project of Youtube clone built using ES6. Implemented
+                import and export and debouncing.
               </p>
               <div>
-              <IoLogoJavascript />
+                <IoLogoJavascript />
                 <DiCss3 />
                 <SiHtml5 />
               </div>
@@ -212,16 +261,16 @@ export const Projects = () => {
           <div className="project">
             <div className="project_videocontainer">
               <div data-aos="fade-right">
-                <img
-                  src="mapapp.png"
-                  alt="weather"
-                />
+                <img src="mapapp.png" alt="weather" />
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
               <h2>Weather</h2>
               <p>
-                Weather app to Search for any location it's gives the current data. Build with the help of weather API and has the weather of the original location details. Built to learn APIs and use them in the project.
+                Weather app to Search for any location it's gives the current
+                data. Build with the help of weather API and has the weather of
+                the original location details. Built to learn APIs and use them
+                in the project.
               </p>
               <div>
                 <SiHtml5 />
