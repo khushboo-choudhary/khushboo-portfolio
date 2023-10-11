9c329650ee4f329deebc76a98c075dbd9bb043b1
khushboo-choudhary <30701525+khushboo-choudhary@users.noreply.github.com>
2023-10-11 19:56:19 +0530

    Some code added.

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Navbar } from "../Navbar/Navbar";
 import { ThemeContext } from "../../Context/theme";
 
 import "./Header.css";
 export const Header = () => {
   const [{ themename }] = React.useContext(ThemeContext);
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const navbar = document.getElementById("navbar");
+      const offset = navbar.offsetTop;
+
+      if (window.pageYOffset > offset) {
+        setIsSticky(true);
+      } else {
+        setIsSticky(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <>
-      <header className={"header center " + themename}>
+      <header
+        id="navbar"
+        className={`header center ${isSticky ? "sticky" : ""} ${themename}`}
+      >
         <h3>
           <a href="#home" className="link">
             Khushboo
